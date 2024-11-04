b66b31f6df65365cbb64235452b4747a6e3b3a0a
Aydahb <ayoub.dahbi.sio@gmail.com>
2024-11-04 23:27:51 +0100

    ajout de classe et sous classe

---
+++ tp11.2/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+public class AbonnementAquaStreet
+{
+    private string nom;
+    private string type;
+    private int duree;
+
+    public string Nom
+    {
+        get { return nom; }
+        set { nom = value; }
+    }
+
+    public string Type
+    {
+        get { return type; }
+        set { type = value; }
+    }
+
+    public int Duree
+    {
+        get { return duree; }
+        set { duree = value; }
+    }
+
+    public AbonnementAquaStreet(string nom, string type, int duree)
+    {
+        Nom = nom;
+        Type = type;
+        Duree = duree;
+    }
+
+    public virtual double CalculerFraisAdhesion()
+    {
+        double tarifBase = 30.0;
+        return tarifBase * duree;
+    }
+
+    public virtual string CheckOffresSpeciales()
+    {
+        return ((type == "Pacha" || type == "pacha") && duree >= 12)
+            ? "Offre spéciale : 9% de réduction pour un abonnement Pacha de 1an ou plus."
+            : "Aucune offre spéciale disponible pour cet abonnement.";
+    }
+
+
+    public virtual void Afficher()
+    {
+        Console.WriteLine("Nom de l'adhérent : " + Nom);
+        Console.WriteLine("Type d'abonnement : " + Type);
+        Console.WriteLine("Durée de l'abonnement : " + Duree + " mois");
+        Console.WriteLine("Frais d'adhésion : " + CalculerFraisAdhesion() + " EUR");
+        Console.WriteLine("Offres spéciales : " + CheckOffresSpeciales());
+    }
+}
+
+public class AbonnementPacha : AbonnementAquaStreet
+{
+    private bool coachPersonnel;
+    private bool accesDetente;
+
+    public bool CoachPersonnel
+    {
+        get { return coachPersonnel; }
+        set { coachPersonnel = value; }
+    }
+
+    public bool AccesDetente
+    {
+        get { return accesDetente; }
+        set { accesDetente = value; }
+    }
+
+    public AbonnementPacha(string nom, string type, int duree, bool coachPersonnel, bool accesDetente)
+        : base(nom, type, duree)
+    {
+        CoachPersonnel = coachPersonnel;
+        AccesDetente = accesDetente;
+    }
+
+    public override double CalculerFraisAdhesion()
+    {
+        double tarifBase = 30.0;
+        double frais = tarifBase * Duree;
+
+        if (CoachPersonnel)
+        {
+            frais += 20.0 * Duree;
+        }
+
+        if (AccesDetente)
+        {
+            frais += 15.0 * Duree;
+        }
+
+        return frais;
+    }
+
+    public override void Afficher()
+    {
+        base.Afficher();
+        Console.WriteLine("Coach personnel : " + (CoachPersonnel ? "Oui" : "Non"));
+        Console.WriteLine("Accès à l'espace détente : " + (AccesDetente ? "Oui" : "Non"));
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AbonnementAquaStreet abonnementStandard = new AbonnementAquaStreet("Ayoub", "Standard", 4);
+        Console.WriteLine("Affichage de l'abonnement standard :");
+        abonnementStandard.Afficher();
+
+        AbonnementPacha abonnementPacha = new AbonnementPacha("Youssef", "Pacha", 12, true, true);
+        Console.WriteLine("\nAffichage de l'abonnement Pacha :");
+        abonnementPacha.Afficher();
+    }
+}
---
+++ tp11/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+public class Animal 
+{
+    private string nom;
+    private string famille;
+    private int age;
+
+    public string Nom 
+    {
+        get { return nom; }
+        set { nom = value; }
+    }
+
+    public string Famille
+    {
+        get { return famille; }
+        set { famille = value; }
+    }
+
+    public int Age 
+    {
+        get { return age; }
+        set { age = value; }
+    }
+
+    public Animal() {}
+
+    public Animal(string nom, string famille, int age)
+    {
+        Nom = nom;
+        Famille = famille;
+        Age = age;
+    }
+
+    public Animal(Animal a)
+    {
+        nom = a.nom;
+        famille = a.famille;
+        age = a.age;
+    }
+
+    public int CalculerAge()
+    {
+        return age * 7;
+    }
+
+    public void AfficherAnimal()
+    {
+        Console.WriteLine("Nom : " + nom);
+        Console.WriteLine("Famille : " + famille);
+        Console.WriteLine("Âge : " + age);
+    }
+
+    public override string ToString()
+    {
+        return $"Nom: {Nom}, Famille: {Famille}, Âge: {Age}";
+    }
+}
+
+public class Chien : Animal
+{
+    private string loisir;
+
+    public string Loisir
+    {
+        get { return loisir; }
+        set { loisir = value; }
+    }
+
+    public Chien(string nom, string famille, int age, string loisir) : base(nom, famille, age)
+    {
+        this.loisir = loisir;
+    }
+
+    public void AfficherChien()
+    {
+        AfficherAnimal();
+        Console.WriteLine("Loisir : " + loisir);
+    }
+}
+
+public class Chat : Animal
+{
+    private int poids;
+
+    public int Poids
+    {
+        get { return poids; }
+        set { poids = value; }
+    }
+
+    public Chat(string nom, string famille, int age, int poids) : base(nom, famille, age)
+    {
+        this.poids = poids;
+    }
+
+    public void AfficherChat()
+    {
+        AfficherAnimal();
+        Console.WriteLine("Poids : " + poids + " kg");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Chien chien = new Chien("Noisette", "Labrador", 5, "manger des croquettes");
+        Chat chat = new Chat("Neige", "Félin", 3, 6);
+       
+
+        Console.WriteLine("Affichage du chien :");
+        chien.AfficherChien();
+      
+         
+
+        Console.WriteLine("\nAffichage du chat :");
+        chat.AfficherChat();
+       
+
+        Console.WriteLine("\nÂge du chien en années humaines : " + chien.CalculerAge());
+        Console.WriteLine("\nÂge du chat en années humaines : " + chat.CalculerAge());
+
+    }
+}
